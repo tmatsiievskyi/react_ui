e83b7d7a2f88972fbea2e53c9ceddcbb8f334be4
tmatsiievskyi1 <matsiievskyi.taras@gmail.com>
2024-08-20 10:32:19 +0300

    init

---
+++ lib/main.ts
@@ -0,0 +1,6 @@
+export { Button } from './components/Button';
+export { Input } from './components/Input';
+
+// export function helloAnything(thing: string): string {
+//   return `Hello ${thing}!`;
+// }
