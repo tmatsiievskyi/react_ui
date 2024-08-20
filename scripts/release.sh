593a925722b4d06fd6b69d0ecf1c5b184a0384e3
tmatsiievskyi1 <matsiievskyi.taras@gmail.com>
2024-08-20 23:07:29 +0300

    RELEASE (serif) : new release v0.0.1

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,7 @@
 current_version=$(node -p "require('./package.json').version")
 echo "Current version: $current_version"
 
-new_version=${npm version --no-git-tag-version patch}
+new_version=$(pnpm version --no-git-tag-version patch)
 echo "New version: $new_version"
 
 $(pnpm run build)
